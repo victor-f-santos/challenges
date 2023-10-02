9644f6d7f8a06d7a77cac6e02844a8af653e50da
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-02 13:26:46 +0200

    values added to the objects

--- objects/js-objects-and-arrays_objects/index.js
+++ objects/js-objects-and-arrays_objects/index.js
@@ -4,8 +4,11 @@ console.clear();
 // Create an object personWithNameAgeEmail with the properties "name", "age" and "email".
 
 const personWithNameAgeEmail = {
-  example: "example",
+  name: "Jay",
+  age: 31,
+  email: "jayscript@gmail.com",
 };
+console.log(personWithNameAgeEmail);
 
 // EXERCISE 2
 // Set the values of name and age equal to the corresponding values of the personAlex object.
@@ -15,8 +18,8 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex.age;
 
 // EXERCISE 3
 // Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.
@@ -26,6 +29,11 @@ const personToChange = {
   age: 24,
 };
 
+personToChange.name = "Alex";
+personToChange.age = 35;
+
+console.log(personToChange);
+
 // EXERCISE 4
 // Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
 
@@ -33,6 +41,11 @@ const petPluto = {
   name: "Pluto",
   species: "dog",
 };
+
+petPluto.age = 5;
+petPluto.breed = "Husky";
+
+console.log(petPluto);
 
 export {
   personWithNameAgeEmail,
