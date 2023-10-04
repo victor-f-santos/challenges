ee9e9cdcd75d4470b5de65f009d3100d1df0bd8e
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-04 13:58:29 +0200

    querySelector in js file added

--- dark-mode/js-basics_dark-mode/js/index.js
+++ dark-mode/js-basics_dark-mode/js/index.js
@@ -1,3 +1,7 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkMode = document.querySelector('[data-js="dark-mode-button"]');
+const lightMode = document.querySelector('[data-js="light-mode-button"]');
+const toogleMode = document.querySelector('[data-js="toggle-button"]');
