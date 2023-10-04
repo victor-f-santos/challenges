54db481194f4578caa651cd5cf5932c5cc7b949b
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-04 17:35:48 +0200

    addEventListener in JS file added. Challenge completed.

--- dark-mode/js-basics_dark-mode/js/index.js
+++ dark-mode/js-basics_dark-mode/js/index.js
@@ -3,5 +3,19 @@ console.clear();
 const bodyElement = document.querySelector('[data-js="body"]');
 
 const darkMode = document.querySelector('[data-js="dark-mode-button"]');
+
 const lightMode = document.querySelector('[data-js="light-mode-button"]');
-const toogleMode = document.querySelector('[data-js="toggle-button"]');
+
+const toggleMode = document.querySelector('[data-js="toggle-button"]');
+
+darkMode.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+lightMode.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+toggleMode.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
