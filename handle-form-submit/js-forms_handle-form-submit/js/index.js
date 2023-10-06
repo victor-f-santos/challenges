0e7d6e087ff9513a19f0a1d64d415ca9dba3663f
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-06 17:02:37 +0200

    challenge completed

--- handle-form-submit/js-forms_handle-form-submit/js/index.js
+++ handle-form-submit/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,24 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const allData = new FormData(event.target);
+  const data = Object.fromEntries(allData);
+
+  console.log(data);
+
+  form.reset();
+
+  event.target.elements.firstName.focus();
+
+  console.log(data.age);
+  console.log(data.badness);
+
+  console.log(
+    `The age-badness-sum of ${data.firstName} is ${
+      Number(data.age) + Number(data.badness)
+    }`
+  );
+});
