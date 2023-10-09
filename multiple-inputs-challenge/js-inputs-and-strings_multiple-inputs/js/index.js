7a426d7782a1aed0ff869de90ef4f63da58e24eb
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-09 14:48:10 +0200

    first commit

--- multiple-inputs-challenge/js-inputs-and-strings_multiple-inputs/js/index.js
+++ multiple-inputs-challenge/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,10 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+button.addEventListener("click", () => {
+  event.preventDefault();
+  firstInput.value = secondInput.value;
+});
+
+secondInput.addEventListener;
