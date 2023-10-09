33deea46503016490ee85f30e8d2d0bbdf4d1949
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-09 13:54:08 +0200

    new entry

--- toast-messages/js-createelement_toast-messages/js/index.js
+++ toast-messages/js-createelement_toast-messages/js/index.js
@@ -5,6 +5,10 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  const newElement = document.createElement("li");
+  newElement.classList.add("toast-container__message");
+  newElement.textContent = "I'm a toast message.";
+  toastContainer.append(newElement);
   // Exercise: Append a new entry to the toast messages container
 });
 
