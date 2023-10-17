d3b5b508e6d3270cb21b0b32ee7f92121888777e
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-17 22:28:53 +0200

    challenge not completed, try catch missing

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,14 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  console.log("Count :", data.count);
+
+  console.log("R2D2 Eye's color: ", data.results[2].eye_color);
+  return data;
+}
 
 fetchData();
