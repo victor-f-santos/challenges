1eff35a6d86d58f58df52bc134697929007f609f
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-18 17:25:15 +0200

    try...catch done

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -3,12 +3,15 @@ console.clear();
 const url = "https://swapi.dev/api/people";
 
 async function fetchData() {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  console.log("Count :", data.count);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    console.log("Count :", data.count);
+  } catch {
+    console.log("R2D2 Eye's color: ", data.results[2].eye_color);
+  }
 
-  console.log("R2D2 Eye's color: ", data.results[2].eye_color);
   return data;
 }
 
