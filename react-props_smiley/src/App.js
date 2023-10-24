68dcb4f842c9360e60aeb7a6641ca266186d040f
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-24 17:29:59 +0200

    challenge done, no emojis though

--- react-props_smiley/src/App.js
+++ react-props_smiley/src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy={false} />;
+}
+
+function Smiley({ isHappy }) {
+  return <p>{isHappy ? "Alright" : "Bleh"}</p>;
 }
