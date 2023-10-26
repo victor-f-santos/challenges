b1cef6706e08a66e1d3260b23f2bb169232c1b42
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-26 15:29:55 +0200

    boxes.js and fragment created

---
+++ react-nesting_fragments/src/components/Boxes.js
@@ -0,0 +1,11 @@
+import Box from "./Box";
+
+export default function Boxes() {
+  return (
+    <>
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </>
+  );
+}
