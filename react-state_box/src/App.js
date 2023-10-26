fe401b7daccaab858e4714cc6611105bff2e5b0c
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-26 15:59:47 +0200

    bonus done

--- react-state_box/src/App.js
+++ react-state_box/src/App.js
@@ -13,7 +13,9 @@ export default function App() {
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>
+        {isActive ? "Active" : "Deactivate"}
+      </button>
     </main>
   );
 }
