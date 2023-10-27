b01b978a02257ce16981339e1a9799f6dc222f19
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-27 16:54:12 +0200

    challenge done

--- react-state-2_table-reservation/src/App.js
+++ react-state-2_table-reservation/src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+  function handleAddPeople() {
+    setPeople(people + 1);
+  }
+
+  function handleRemovePeople() {
+    setPeople(people - 1);
+  }
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        onAddPeople={handleAddPeople}
+        onRemovePeople={handleRemovePeople}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
