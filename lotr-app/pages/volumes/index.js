85cc3d7c81b1e903a1babad8acea65b9abf8b8d0
VICTOR SANTOS <victor.f089@gmail.com>
2023-11-09 16:13:22 +0100

    detail page content done, no bonus

--- lotr-app/pages/volumes/index.js
+++ lotr-app/pages/volumes/index.js
@@ -1,11 +1,19 @@
 import { introduction } from "@/resources/lib/data";
 import Link from "next/link";
+import { volumes } from "@/resources/lib/data";
 
 export default function HomePage() {
+  const currentVolume = volumes.find(
+    ({ slug }) => slug === "the-fellowship-of-the-ring"
+  );
+
+  if (!currentVolume) {
+    return null;
+  }
   return (
     <div>
       <h1>Lord of the Rings</h1>
-
+      <p>{introduction}</p>
       <h2>All Volumes</h2>
       <ul>
         <li>
